2efe50ce7b26b57ad09e9283aef72a102790087c
Asbjorn Aarekol <asbjaare@gmail.com>
2023-10-05 15:10:27 +0200

    final release

--- data/plot.py
+++ data/plot.py
@@ -6,23 +6,39 @@ import numpy as np
 data = pd.read_csv("results.csv")
 
 # Group the data by 'nodes' and 'signal' and calculate the mean and standard deviation
-grouped = data.groupby(["nodes", "signal"])["rps"].agg([np.mean, np.std]).reset_index()
+grouped = data.groupby(["nodes", "signal"])["rps"].agg(["mean", "std"]).reset_index()
 
 # Separate 'put' and 'get' data
 put_data = grouped[grouped["signal"] == "put"]
 get_data = grouped[grouped["signal"] == "get"]
-
+print(put_data["std"].to_list())
 # Create a plot
 plt.figure(figsize=(10, 6))
 
 # Plot 'put' data with error bars for standard deviation
 plt.errorbar(
-    put_data["nodes"], put_data["mean"], yerr=put_data["std"], label="Put", marker="o"
+    put_data["nodes"],
+    put_data["mean"],
+    yerr=put_data["std"],
+    label="Put",
+    marker="o",
+    elinewidth=15,
+    capsize=10,
+    capthick=5,
+    linewidth=5,
 )
 
 # Plot 'get' data with error bars for standard deviation
 plt.errorbar(
-    get_data["nodes"], get_data["mean"], yerr=get_data["std"], label="Get", marker="o"
+    get_data["nodes"],
+    get_data["mean"],
+    yerr=get_data["std"],
+    label="Get",
+    marker="o",
+    elinewidth=15,
+    capsize=10,
+    capthick=5,
+    linewidth=5,
 )
 
 # Add labels and a legend
@@ -30,7 +46,7 @@ plt.xlabel("Nodes")
 plt.ylabel("Average RPS")
 plt.title("Average RPS vs. Nodes (with Standard Deviation)")
 plt.legend()
-
+plt.xticks([1, 2, 4, 8, 16])
 # Show the plot
 plt.grid(True)
-plt.show()
+# plt.show()
