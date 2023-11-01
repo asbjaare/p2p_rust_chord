561bb5ac2d7e7519264051d984db1ce4bcd7a13e
aaa040 <aaa040@ificluster.ifi.uit.no>
2023-11-01 20:30:00 +0100

    done with testing

--- test/get.py
+++ test/get.py
@@ -1,68 +1,26 @@
 import requests
 from time import sleep as wait
-import random
 import time
-
-# key = input("what key:")
-nprime = "172.21.21.175:55552"
-
-def get_random_keys(url, num_keys, key_length):
-    start_time = time.time()
-    for i in range(num_keys):
-        key = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=key_length))
-        response = requests.get(f"{url}/{key}")
-        if response.status_code != 200:
-            print(f"Error getting key {key}: {response.status_code}")
-    end_time = time.time()
-    throughput = num_keys / (end_time - start_time)
-    print(f"Got {num_keys} keys in {end_time - start_time:.2f} seconds, throughput: {throughput:.2f} keys/sec")
 
     
 start = time.time()
-
+nodes = 5
 i = 1;
 with open ("ip.txt", "r") as myfile:
 
-    data=myfile.readlines()[:49]
+    data=myfile.readlines()[:nodes]
     for line in data:
         ip_addr = line.rstrip()
      
-        print(i)
-        response = requests.post("http://"+ip_addr.split(" ")[2]+":60009/leave")
+        # print(i)
+        response = requests.post("http://"+ip_addr.split(" ")[2]+":61021/leave")
         i = i + 1
-        print(response.status_code)
-        print(response.text)
+        # print(response.status_code)
+        # print(response.text)
         # wait(0.5)
 
 end = time.time()
 
-print(end - start)
+print(f"{end - start},{nodes*2}")
 
 
-# response = requests.get("http://172.21.21.181:55550/succ_list")
-# # response = requests.post("http://172.21.21.175:55555/leave")
-# # response = requests.get("http://172.21.21.176:55555/storage/1", data="hello world")
-# # put_random_keys("http://172.21.21.176:55555/storage", 1000, 10)
-# # get_random_keys("http://172.21.21.176:55555/storage", 1000, 10)
-# # response = requests.get("http://172.21.21.175:55555/node-info")
-# print(response.status_code)
-# print(response.text)
-
-# neighbours = []
-# with open ("ip.txt", "r") as myfile:
-
-#     data=myfile.readlines()[:20]
-
-#     for line in data:
-#         ip_addr = line.rstrip()
-   
-#         print(ip_addr.split(" ")[2])
-
-#         response = requests.get("http://"+ip_addr.split(" ")[2]+":55557/neighbors")
-
-#         wait(1)
-
-
-# response = requests.get("http://172.21.21.126:60021/storage/7d20658c-87b9-48a0-9624-2fd7d42ece26")
-# print(response.status_code)
-# print(response.text)
